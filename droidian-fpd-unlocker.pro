1c9fec64d3307182c19eda442199850a835d5c1e
Bardia Moshiri <fakeshell@bardia.tech>
2023-07-01 20:30:42 +0000

    add droidian-fpd-unlocker

---
+++ droidian-fpd-unlocker.pro
@@ -0,0 +1,9 @@
+TARGET = droidian-fpd-unlocker
+
+QT += dbus
+
+SOURCES += src/fpdunlocker.cpp \
+    src/fpdinterface.cpp
+
+HEADERS += \
+    src/fpdinterface.h
---
+++ src/fpdunlocker.cpp
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (C) 2023 Bardia Moshiri <fakeshell@bardia.tech>
+
+#include <QCoreApplication>
+#include <QTextStream>
+#include <QDebug>
+#include <QProcess> // Include QProcess
+#include "fpdinterface.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    FPDInterface fpdInterface;
+
+    QObject::connect(&fpdInterface, &FPDInterface::identified, [](const QString &finger) {
+        qDebug() << "Identified finger:" << finger;
+        QProcess *process = new QProcess();
+        process->start("loginctl", QStringList() << "unlock-session" << "c1");
+
+        QObject::connect(process, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
+            [&](int exitCode, QProcess::ExitStatus exitStatus) {
+                QCoreApplication::exit(exitCode);
+            });
+    });
+
+    QObject::connect(&fpdInterface, &FPDInterface::errorInfo, [](const QString &info) {
+        qDebug() << "Error info:" << info;
+        QCoreApplication::exit(1);
+    });
+
+    qDebug() << "Waiting for finger identification...";
+    fpdInterface.identify();
+
+    return app.exec();
+}
